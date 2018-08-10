9cd92685161bf6d9ea9f275a23831ee6e04784ec
Nikita Nyatin <yesnik@yandex.ru>
2018-08-10 10:52:43 +0500

    Add robot model and specs

---
+++ lib/robot.rb
@@ -0,0 +1,67 @@
+class Robot
+  module Directions
+    NORTH = 'NORTH'
+    EAST = 'EAST'
+    SOUTH = 'SOUTH'
+    WEST = 'WEST'
+
+    LIST = [NORTH, EAST, SOUTH, WEST]
+  end
+
+  attr_accessor :x, :y, :direction
+
+  class UndefinedDirectionError < StandardError; end
+
+  def place(x, y, direction)
+    validate_direction(direction)
+
+    @x = x
+    @y = y
+    @direction = direction
+  end
+
+  def move
+    case direction
+    when Directions::NORTH
+      @y += 1
+    when Directions::SOUTH
+      @y -= 1
+    when Directions::EAST
+      @x += 1
+    when Directions::WEST
+      @x -= 1
+    end
+  end
+
+  def left
+    next_index = current_direction_index - 1
+    next_index = (directions_amount - 1) if (next_index < 0)
+
+    @direction = Directions::LIST[next_index]
+  end
+
+  def right
+    next_index = current_direction_index + 1
+    next_index = 0 if (next_index >= directions_amount)
+
+    @direction = Directions::LIST[next_index]
+  end
+
+  def report
+    [x, y, direction]
+  end
+
+  protected
+
+  def validate_direction(direction)
+    raise UndefinedDirectionError unless Directions::LIST.include?(direction)
+  end
+
+  def current_direction_index
+    Directions::LIST.index(direction)
+  end
+
+  def directions_amount
+    @directions_amount ||= Directions::LIST.size
+  end
+end
---
+++ spec/robot_spec.rb
@@ -0,0 +1,136 @@
+require 'spec_helper'
+require './lib/robot'
+
+describe Robot do
+  subject { described_class.new }
+
+  describe '#place' do
+    context 'when placed at 1, 2, NORTH' do
+      before { subject.place(1, 2, 'NORTH') }
+      it { expect(subject.x).to eq 1 }
+      it { expect(subject.y).to eq 2 }
+      it { expect(subject.direction).to eq 'NORTH' }
+    end
+
+    context 'when placed at unknown direction' do
+      it { expect { subject.place(0, 0, 'SOME_DIRECTION') }.
+        to raise_error Robot::UndefinedDirectionError }
+    end
+  end
+
+  describe '#left' do
+    context 'when current direction is NORTH' do
+      before do
+        subject.place(0, 0, 'NORTH')
+        subject.left
+      end
+
+      it 'rotates the robot 90 degrees counterclockwise' do
+        expect(subject.direction).to eq 'WEST'
+      end
+    end
+
+    context 'when we call method 4 times' do
+      before do
+        subject.place(0, 0, 'SOUTH')
+        4.times { subject.left }
+      end
+
+      it 'looks in the same direction' do
+        expect(subject.direction).to eq 'SOUTH'
+      end
+    end
+  end
+
+  describe '#right' do
+    context 'when current direction is NORTH' do
+      before do
+        subject.place(0, 0, 'NORTH')
+        subject.right
+      end
+
+      it 'rotates the robot 90 degrees clockwise' do
+        expect(subject.direction).to eq 'EAST'
+      end
+    end
+
+    context 'when we call method 4 times' do
+      before do
+        subject.place(0, 0, 'SOUTH')
+        4.times { subject.right }
+      end
+
+      it 'looks in the same direction' do
+        expect(subject.direction).to eq 'SOUTH'
+      end
+    end
+  end
+
+  describe '#move' do
+    let(:x) { 2 }
+    let(:y) { 3 }
+
+    context 'when direction is NORTH' do
+      
+      before do
+        subject.place(x, y, 'NORTH')
+        subject.move
+      end
+
+      it 'increases y by 1' do
+        expect(subject.y).to eq (y + 1)
+      end
+
+      context 'and wnen we call method twice' do
+        before { subject.move }
+
+        it 'increases y by 2' do
+          expect(subject.y).to eq (y + 2)
+        end
+      end
+    end
+
+    context 'when direction is SOUTH' do
+      before do
+        subject.place(x, y, 'SOUTH')
+        subject.move
+      end
+
+      it 'decreases y by 1' do
+        expect(subject.y).to eq (y - 1)
+      end
+    end
+
+    context 'when direction is WEST' do
+      before do
+        subject.place(x, y, 'WEST')
+        subject.move
+      end
+
+      it 'decreases x by 1' do
+        expect(subject.x).to eq (x - 1)
+      end
+    end
+
+    context 'when direction is EAST' do
+      before do
+        subject.place(x, y, 'EAST')
+        subject.move
+      end
+
+      it 'increases x by 1' do
+        expect(subject.x).to eq (x + 1)
+      end
+    end
+  end
+
+  describe '#report' do
+    before do
+      subject.place(2, 3, 'NORTH')
+    end
+
+    it 'returns array with current robot position' do
+      expect(subject.report).to eq [2, 3, 'NORTH']
+    end
+  end
+end
